a5577628b686f6833a12e95f23c17ebe70b154a6
MinKuk Jo <strike0115@naver.com>
2020-02-28 04:56:33 +0900

    Initial commit

---
+++ src/main/java/me/harry/springbootgettingstarted/SpringBootGettingstartedApplication.java
@@ -0,0 +1,11 @@
+package me.harry.springbootgettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGettingstartedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGettingstartedApplication.class, args);
+    }
+}
---
+++ src/test/java/me/harry/springbootgettingstarted/SpringBootGettingstartedApplicationTests.java
@@ -0,0 +1,13 @@
+package me.harry.springbootgettingstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGettingstartedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
